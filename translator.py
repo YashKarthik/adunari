1a70f37e88e8bb300ae77dfbf636c07ab719096e
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-06-14 01:33:52 -0700

    imore

--- translator.py
+++ translator.py
@@ -13,9 +13,6 @@ class AsmTranslator:
             ret = self.dest_bit[m] | ret
         return ret;
 
-    def comp(self, mnemonic: str) -> int:
-        return 0b10
-
     jump_bits = {
         None: 0b00,
         "JGT": 0b001,
@@ -28,3 +25,29 @@ class AsmTranslator:
     }
     def jump(self, mnemonic: str) -> int:
         return self.jump_bits[mnemonic]
+
+    """
+        [ ] c6
+    """
+    def comp(self, mnemonic: str) -> int:
+        ret = 0b0000000
+        if mnemonic.find("M") != -1:
+            ret = ret | 0b1000000 # a
+
+        if mnemonic.find("D") == -1:
+            ret = ret | 0b0100000 # c1
+
+        if mnemonic.find("0") == -1 and (mnemonic.find("D") == -1 or (mnemonic.find("D") != -1 and mnemonic.find("+") and mnemonic.find("1")) or mnemonic.find("D-A") or mnemonic.find("D|A")):
+            ret = ret | 0b0010000 # c2
+
+        if mnemonic.find("A") == -1 and mnemonic.find("M") == -1:
+            ret = ret | 0b0001000 # c3
+
+        if mnemonic.find("0") == -1 and mnemonic.find("-1") == -1 and (mnemonic.find("A") == -1 or (mnemonic.find("A") != -1 and mnemonic.find("+") and mnemonic.find("1")) or mnemonic.find("A-D") or mnemonic.find("D|A")):
+            ret = ret | 0b0000100 # c4
+
+        if mnemonic.find("1") or mnemonic.find("0") or mnemonic.find("+") or mnemonic.find("-"):
+            ret = ret | 0b0000010 # c5
+
+        return ret
+
